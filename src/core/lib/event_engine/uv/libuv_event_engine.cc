89df1a2fcfe7af6a34e2ad6b810b23acb3122b5e
drfloob <drfloob@users.noreply.github.com>
2021-10-05 18:04:12 +0000

    Automated change: Fix sanity tests

--- src/core/lib/event_engine/uv/libuv_event_engine.cc
+++ src/core/lib/event_engine/uv/libuv_event_engine.cc
@@ -42,7 +42,7 @@ namespace {
 
 struct SchedulingRequest : grpc_core::MultiProducerSingleConsumerQueue::Node {
   typedef std::function<void(LibuvEventEngine*)> functor;
-  SchedulingRequest(functor&& f) : f(std::move(f)) {}
+  explicit SchedulingRequest(functor&& f) : f(std::move(f)) {}
   functor f;
 };
 
@@ -234,7 +234,7 @@ void LibuvEventEngine::RunThread() {
   sigset_t set;
   sigemptyset(&set);
   sigaddset(&set, SIGPIPE);
-  pthread_sigmask(SIG_BLOCK, &set, NULL);
+  pthread_sigmask(SIG_BLOCK, &set, nullptr);
 
   // Setting up the loop.
   worker_thread_id_ = std::this_thread::get_id();
