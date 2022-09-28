7f28546131f86b18afb68c03958614e00cd66caf
drfloob <drfloob@users.noreply.github.com>
2022-09-28 22:18:41 +0000

    Automated change: Fix sanity tests

--- src/core/ext/filters/client_channel/lb_policy/xds/xds_cluster_manager.cc
+++ src/core/ext/filters/client_channel/lb_policy/xds/xds_cluster_manager.cc
@@ -377,7 +377,7 @@ void XdsClusterManagerLb::UpdateStateLocked() {
         break;
       }
       default:
-        GPR_UNREACHABLE_CODE(return);
+        GPR_UNREACHABLE_CODE(return );
     }
   }
   // Determine aggregated connectivity state.
