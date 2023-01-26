306c16fcba7115d410b86ef3a197e59ecf0f5e6a
drfloob <drfloob@users.noreply.github.com>
2023-01-26 01:29:30 +0000

    Automated change: Fix sanity tests

--- src/core/lib/event_engine/windows/windows_listener.cc
+++ src/core/lib/event_engine/windows/windows_listener.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <grpc/support/port_platform.h>
+
 #include "src/core/lib/event_engine/windows/windows_listener.h"
 
 #include "absl/status/status.h"
@@ -116,7 +118,7 @@ class SinglePortSocketListener {
           gpr_log(GPR_ERROR, "invalid peer name: %s",
                   addr_uri.status().ToString().c_str());
         } else {
-            peer_name_string = *addr_uri;
+          peer_name_string = *addr_uri;
         }
       }
 
