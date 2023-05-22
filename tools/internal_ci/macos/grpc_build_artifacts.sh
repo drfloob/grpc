bc1ad2ba5219a90261ae5eb2e92a96e8f1eccf9c
AJ Heller <hork@google.com>
2023-05-22 16:04:11 -0700

    [ci] Remove unnecessary preparation steps from no-op artifact builds

--- tools/internal_ci/macos/grpc_build_artifacts.sh
+++ tools/internal_ci/macos/grpc_build_artifacts.sh
@@ -13,16 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -ex
-
-# avoid slow finalization after the script has exited.
-source $(dirname $0)/../../../tools/internal_ci/helper_scripts/move_src_tree_and_respawn_itself_rc
-
-# change to grpc repo root
-cd $(dirname $0)/../../..
-
-source tools/internal_ci/helper_scripts/prepare_build_macos_rc
-
 echo "The grpc_build_artifacts -> grpc_build_packages -> grpc_distribtests build flow has been deprecated."
 echo "To build the packages for a grpc release, use the grpc_build_all_packages job."
 echo "(which relies on the per-language distribtests jobs to build and test the packages)."
