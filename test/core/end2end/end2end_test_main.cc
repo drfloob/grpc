671ad86c877e71e21db77fb0d13c66b3b14c35cd
AJ Heller <hork@google.com>
2023-08-02 13:26:40 -0700

    force EE trace in core end2end tests

--- test/core/end2end/end2end_test_main.cc
+++ test/core/end2end/end2end_test_main.cc
@@ -36,6 +36,7 @@ int main(int argc, char** argv) {
   // TODO(ctiller): make this per fixture?
   grpc_core::ConfigVars::Overrides overrides;
   overrides.default_ssl_roots_file_path = CA_CERT_PATH;
+  overrides.trace = "event_engine,event_engine_endpoint,event_engine_endpoint_data,event_engine_poller";
   grpc_core::ConfigVars::SetOverrides(overrides);
   const auto all_tests = grpc_core::CoreEnd2endTestRegistry::Get().AllTests();
   for (const auto& test : all_tests) {
