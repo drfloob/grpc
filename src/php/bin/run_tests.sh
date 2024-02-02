e86900c0cdee15977625c877542338127844385b
Stanley Cheung <stanleycheung@google.com>
2024-02-02 21:48:08 +0000

    Add debug message

--- src/php/bin/run_tests.sh
+++ src/php/bin/run_tests.sh
@@ -38,3 +38,6 @@ if [[ "$SKIP_PERSISTENT_CHANNEL_TESTS" != "true" ]]; then
    $(which php) $extension_dir -d max_execution_time=300 $(which phpunit) -v --debug \
      ../tests/unit_tests/PersistentChannelTests
 fi
+
+echo "END OF PHP run_tests"
+
