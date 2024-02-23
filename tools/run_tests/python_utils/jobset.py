8c768e1b91e26f9e7bd28384f20dda60b34b968a
AJ Heller <hork@google.com>
2024-02-23 17:34:50 +0000

    oops

--- tools/run_tests/python_utils/jobset.py
+++ tools/run_tests/python_utils/jobset.py
@@ -322,7 +322,7 @@ class Job(object):
         try_start = lambda: subprocess.Popen(
             args=cmdline,
             stderr=subprocess.STDOUT,
-            stdout=sys.stdout,
+            stdout=self._logfile,
             cwd=self._spec.cwd,
             shell=self._spec.shell,
             env=env,
