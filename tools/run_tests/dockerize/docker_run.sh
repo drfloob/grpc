062a1c7aadd235188a2c3edeb8f69642073abda5
AJ Heller <hork@google.com>
2024-02-23 00:15:08 +0000

    x

--- tools/run_tests/dockerize/docker_run.sh
+++ tools/run_tests/dockerize/docker_run.sh
@@ -18,15 +18,14 @@
 
 set -e
 
-if [ "${RELATIVE_COPY_PATH}" == "" ]
-then
+if [ "${RELATIVE_COPY_PATH}" == "" ]; then
   mkdir -p /var/local/git
-  git clone "${EXTERNAL_GIT_ROOT}" /var/local/git/grpc
+  git clone --depth=1 "${EXTERNAL_GIT_ROOT}" /var/local/git/grpc
   # clone gRPC submodules, use data from locally cloned submodules where possible
   # TODO: figure out a way to eliminate this following shellcheck suppressions
   # shellcheck disable=SC2016,SC1004
-  (cd "${EXTERNAL_GIT_ROOT}" && git submodule foreach 'git clone ${EXTERNAL_GIT_ROOT}/${name} /var/local/git/grpc/${name}')
-  (cd /var/local/git/grpc && git submodule init)
+  # (cd "${EXTERNAL_GIT_ROOT}" && git submodule foreach 'git clone ${EXTERNAL_GIT_ROOT}/${name} /var/local/git/grpc/${name}')
+  # (cd /var/local/git/grpc && git submodule init)
 else
   mkdir -p "/var/local/git/grpc/${RELATIVE_COPY_PATH}"
   cp -r "${EXTERNAL_GIT_ROOT}/${RELATIVE_COPY_PATH}"/* "/var/local/git/grpc/${RELATIVE_COPY_PATH}"
@@ -37,16 +36,14 @@ cd /var/local/git/grpc
 # whatever is written to the reports/ dir will be made available outside of the docker container.
 ln -s "${EXTERNAL_GIT_ROOT}/reports" reports
 # if OUTPUT_DIR is specified, whatever is written to ./${OUTPUT_DIR}/ will be made available outside of the docker container.
-if [ "${OUTPUT_DIR}" != "" ]
-then
+if [ "${OUTPUT_DIR}" != "" ]; then
   ln -s "${EXTERNAL_GIT_ROOT}/${OUTPUT_DIR}" "${OUTPUT_DIR}"
 fi
 
 exit_code=0
 ${DOCKER_RUN_SCRIPT_COMMAND} || exit_code=$?
 
-if [ -x "$(command -v ccache)" ]
-then
+if [ -x "$(command -v ccache)" ]; then
   ccache --show-stats || true
 fi
 
