f4479de4dae28610a400e3a0a65e80c08159a2d2
drfloob <295906+drfloob@users.noreply.github.com>
2024-03-25 19:30:11 +0000

    Automated change: Fix sanity tests

--- src/core/ext/transport/chttp2/server/chttp2_server.h
+++ src/core/ext/transport/chttp2/server/chttp2_server.h
@@ -64,7 +64,7 @@ class PassiveListenerImpl final : public PassiveListener {
  private:
   // note: the grpc_core::Server redundant namespace qualification is
   // required for older gcc versions.
-  friend absl::Status(::grpc_server_add_passive_listener)(
+  friend absl::Status(grpc_server_add_passive_listener)(
       grpc_core::Server* server, grpc_server_credentials* credentials,
       std::shared_ptr<grpc_core::experimental::PassiveListenerImpl>
           passive_listener);
